b241a8e52c8af19b13a91a2be38878304e501e5c
Sergey Bronzza <repinetskiy@gmail.com>
2019-05-18 21:31:48 +0300

    Initial commit

---
+++ HomeWork20MVC/src/main/java/MateAcad/HomeWork20MVC/HomeWork20MvcApplication.java
@@ -0,0 +1,13 @@
+package MateAcad.HomeWork20MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork20MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork20MvcApplication.class, args);
+	}
+
+}
